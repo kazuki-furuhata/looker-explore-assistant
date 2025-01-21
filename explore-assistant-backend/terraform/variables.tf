f8b0d8752c5c2ade5a18fbae17588bf705b192ca
Furuhata <furuhata.kazuki@tbs.co.jp>
2025-01-21 06:29:32 +0000

    commit

--- explore-assistant-backend/terraform/variables.tf
+++ explore-assistant-backend/terraform/variables.tf
@@ -1,4 +1,3 @@
-
 #
 # REQUIRED VARIABLES
 #
@@ -6,11 +5,12 @@
 variable "project_id" {
   type = string
   description = "GCP Project ID"
+  default = "tbs-dmp"
 }
 
 variable "use_cloud_function_backend" {
   type = bool
-  default = false
+  default = true
 }
 
 variable "use_bigquery_backend" {
@@ -25,7 +25,7 @@ variable "use_bigquery_backend" {
 variable "deployment_region" {
   type = string
   description = "Region to deploy the Cloud Run service. Example: us-central1"
-  default = "us-central1"
+  default = "asia-northeast1"
 }
 
 variable "cloud_run_service_name" {
@@ -41,4 +41,3 @@ variable "dataset_id_name" {
     type = string
     default = "explore_assistant"
 }
-
